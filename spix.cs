8f47cb2e5fc1c29c3ce5363a39ba7a54e3f74fd8
Jiwon <jiwon1.han@gmail.com>
2020-02-27 12:46:00 -0800

    Complete

--- spix.cs
+++ spix.cs
@@ -1,87 +1,87 @@
 using System;
+using System.Collections.Generic;
 
 class Spynx {
-    static void Main()
-    {   
-        Console.WriteLine("Main Game");
-        Console.WriteLine("Do you want to start riddle game? Please say Y if you are okay");
-        string answer = Console.ReadLine().ToLower();
 
-        if (answer == "y") 
-        {
-            Riddle();
-        }
-        else
-        {
-            Console.WriteLine("Good bye");
-        }
-    }
-    static void Riddle() 
+    public static Dictionary<string, string> riddleAnswer = new Dictionary<string, string>() {};
+
+    static void Main()
     {
-            Console.WriteLine("What has to be broken before you can use it?");
+
+
+      string riddleOne = "I’m tall when I’m young, and I’m short when I’m old. What am I?";
+      string riddleTwo = "What has to be broken before you can use it?";
+      string riddleThree = "What is full of holes but still holds water?";
+      string riddleFour = "What is always in front of you but can’t be seen?";
+
+      string answerOne = "A candle";
+      string answerTwo = "An egg";
+      string answerThree = "A sponge";
+      string answerFour = "The future";
+
+      riddleAnswer.Add(riddleOne, answerOne.ToLower());
+      riddleAnswer.Add(riddleTwo, answerTwo.ToLower());
+      riddleAnswer.Add(riddleThree, answerThree.ToLower());
+      riddleAnswer.Add(riddleFour, answerFour.ToLower());
+
+      MainGame();
+    }
+
+    static int RandomNumber(int num)
+    {
+      Random random = new Random();
+      int index = random.Next(num);
+      return index;
+    }
+
+    static void MainGame()
+    {
+      Console.WriteLine("Main Game");
+      Console.WriteLine("Would you like to play a riddle game with Spynx? [Yes for Y / No for N]");
+      string answer = Console.ReadLine().ToLower();
+
+      if (answer == "y")
+      {
+          Riddle();
+      }
+      else
+      {
+          Console.WriteLine("Goodbye");
+      }
+    }
+
+    static void Riddle()
+    {
+            List<string> riddleList = new List<string>(riddleAnswer.Keys);
+            string randomRiddle = riddleList[RandomNumber(riddleList.Count)];
+            Console.WriteLine(randomRiddle);
             string answerRiddle = Console.ReadLine().ToLower();
 
-            // int index = Random.Next(list.Count);
-            // Console.WriteLine();
 
-            if (answerRiddle == "egg" || answerRiddle == "an egg")
+            if (riddleAnswer[randomRiddle] == answerRiddle )
             {
-                Riddles();
+                Console.WriteLine("You win!")
                 PlayAgain();
             }
             else
             {
                 Console.WriteLine("Answer is not correct. You are my tomorrow breakfast");
             }
-        
+
     }
 
     static void PlayAgain()
     {
-        Console.WriteLine("Would you like to play again?");
+        Console.WriteLine("Would you like to play again? [Yes for Y / No for N]");
         string userAnswer = Console.ReadLine().ToLower();
         if (userAnswer == "y")
         {
             Riddle();
         }
-        else 
+        else
         {
             Console.WriteLine("Bye");
         }
     }
 
-    static void Riddles()
-    {
-        List<string> riddles = new List<string>{};
-        string riddleOne = "I’m tall when I’m young, and I’m short when I’m old. What am I?";
-        string riddleTwo = "I’m tall when I’m young, and I’m short when I’m old. What am I?";
-        string riddleThree = "What is full of holes but still holds water?";
-        string riddleFour = "What is always in front of you but can’t be seen?";
-
-        riddles.Add(riddleOne);
-        riddles.Add(riddleTwo);
-        riddles.Add(riddleThree);
-        riddles.Add(riddleFour);
-        Console.WriteLine(riddles[0]);
-
-
-        List<string> answers = new List<string>{};
-        string answerOne = "A candle";
-        string answerTwo = "An egg";
-        string answerThree = "A sponge";
-        string answerFour = "The future";
-
-        answers.Add(answerOne);
-        answers.Add(answerTwo);
-        answers.Add(answerThree);
-        answers.Add(answerFour);
-        return answers;
-
-        Dictionary <string, string> riddleAnswer = new Dictionary<string, string>() {};
-
-        foreach (string riddle in riddles)
-        {
-            riddleAnswer.Add(riddle);
-        }
-    }
 }
